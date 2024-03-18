a502a9432bf526ad380891fd6e8ee0f8dd7a5ab7
va1ent0n46 <3t9o9n9@gmail.com>
2024-03-18 11:34:16 +0700

    fix some error

--- src/routes/Todo.ts
+++ src/routes/Todo.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post("/create", controller.createTodo);
 router.get("/get/:todoId", controller.getTodo);
 router.get("/get", controller.getAllTodo);
-router.post("/update/:todoId", controller.updateTodo);
-router.post("/delete/:todoId", controller.deleteTodo);
+router.patch("/update/:todoId", controller.updateTodo);
+router.delete("/delete/:todoId", controller.deleteTodo);
 
 export = router;
