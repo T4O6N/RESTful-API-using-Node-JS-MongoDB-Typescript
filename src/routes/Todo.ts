f18dd21de3b1b61659a3a582e444ac68be48ac09
va1ent0n46 <3t9o9n9@gmail.com>
2024-03-18 11:24:28 +0700

    fix some error

--- src/routes/Todo.ts
+++ src/routes/Todo.ts
@@ -4,8 +4,8 @@ import controller from "../controllers/Todo";
 const router = express.Router();
 
 router.post("/create", controller.createTodo);
-router.post("/get/:todoId", controller.getTodo);
-router.post("/get/", controller.getAllTodo);
+router.get("/get/:todoId", controller.getTodo);
+router.get("/get", controller.getAllTodo);
 router.post("/update/:todoId", controller.updateTodo);
 router.post("/delete/:todoId", controller.deleteTodo);
 
