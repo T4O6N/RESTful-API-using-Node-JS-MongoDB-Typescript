5b0038f3c125765cc6aad047c7236ccc01d9cd65
va1ent0n46 <3t9o9n9@gmail.com>
2024-03-20 22:56:56 +0700

    add middleware: ValidateSchema

--- src/routes/Todo.ts
+++ src/routes/Todo.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import controller from "../controllers/Todo";
+import { Schemas, ValidateSchema } from "../middleware/ValidateSchema";
 
 const router = express.Router();
 
-router.post("/create", controller.createTodo);
+router.post("/create", ValidateSchema(Schemas.todo.create), controller.createTodo);
 router.get("/get/:todoId", controller.getTodo);
 router.get("/get", controller.getAllTodo);
-router.patch("/update/:todoId", controller.updateTodo);
+router.patch("/update/:todoId", ValidateSchema(Schemas.todo.update), controller.updateTodo);
 router.delete("/delete/:todoId", controller.deleteTodo);
 
 export = router;
